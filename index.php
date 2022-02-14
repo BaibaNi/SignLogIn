52cba55bc1ec0919264928e1d87481f67fa1b37c
Baiba <baiba.niparte@gmail.com>
2022-02-14 15:55:39 +0200

    Sign-in/Login task

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Sign-in & Log-in</title>
+</head>
+
+<h1><b>SIGN-IN & LOG-IN</b></h1>
+
+<body>
+
+</body>
+</html>
